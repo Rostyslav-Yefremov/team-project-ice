2782697a638c8c6b53b696487a941f04e890f6f5
Oleksandr Honcharuk <sashko.goncharuk@gmail.com>
2021-07-18 19:58:15 +0300

    update header & mobile menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './sass/main.scss';
+
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.mobile-open'),
+    closeMenuBtn: document.querySelector('.mobile-close'),
+    menu: document.querySelector('.mobile-menu'),
+  };
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+  }
+})();
